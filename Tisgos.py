d9c19e8df86a9a67d6a62638678d889e1152982b
lleonardoolive <lleonardooliv@outlook.com>
2026-08-29 17:16:35 -0300

    Descrição a toa

--- Tisgos.py
+++ Tisgos.py
@@ -1,1 +1,11 @@
-print("Cu de imbigo!")
+import secrets
+
+
+def gerar_codigo_6_digitos():
+    # Gera um número entre 100000 e 999999
+    return str(secrets.randbelow(900000) + 100000)
+
+
+if __name__ == "__main__":
+    codigo = gerar_codigo_6_digitos()
+    print(f"Código gerado: {codigo}")
